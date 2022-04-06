fd74b6a99a863b69e99c1bb48b624c43102de3e1
Owais-AlShishani <98020718+Owais-AlShishani@users.noreply.github.com>
2022-04-06 14:53:59 +0300

    Add project files.

---
+++ Services/Controllers/ServiceController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Services.Models;
+
+namespace Services.Controllers
+{
+    public class ServiceController : Controller
+    {
+        // GET: Service
+        public ActionResult Index()
+        {
+            var service = new Service()
+            {
+                ServiceId = 1,
+                ServiceName = "Passport",
+                ServiceDuration = 3,
+                ServiceCost = 12,
+                Active = true
+            };
+
+            return View(service);
+        }
+    }
+}
---
+++ Services/Models/Service.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Services.Models
+{
+    public class Service
+    {
+        public int ServiceId { get; set; }
+        public string ServiceName { get; set; }
+        public int ServiceDuration { get; set; }
+        public int ServiceCost { get; set; }
+        public bool Active { get; set; }
+    }
+}
